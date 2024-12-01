5d72499e0558080d2bc42a548e3dc3d91d68ede4
ailre <xtramainpp@gmail.com>
2024-12-01 16:48:30 +0900

    init linux home setup

---
+++ init.sh
@@ -0,0 +1,8 @@
+# append ailre-bashrc if not exist
+
+# install git curl build-essentials nvim
+# add .ssh config 
+# install ripgrep ts_ls lua_ls 
+# install deno node(for npm) rustc
+# clone nvim config
+#  maybe go?

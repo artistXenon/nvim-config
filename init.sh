c3285cb5ac6c24eb9db4b36e774460353859272e
ailre <xtramainpp@gmail.com>
2024-12-02 08:21:47 +0900

    update deps

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 # append ailre-bashrc if not exist
 
-# install git curl build-essentials nvim
+# install git curl build-essentials nvim zip unzip
 # add .ssh config 
-# install ripgrep ts_ls lua_ls 
+# install lazygit ripgrep ts_ls lua_ls deno
 # install deno node(for npm) rustc
 # clone nvim config
 #  maybe go?

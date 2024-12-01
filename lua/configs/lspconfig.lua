cffb43d18f9a1086de59909bf4f3260ee4eca800
artistXenon <xtramainpp@gmail.com>
2024-12-01 15:10:07 +0900

    lsp config up to date

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "ts_ls" }
+local servers = { "html", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -16,10 +16,27 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- configuring single server, example: typescript
+
 lspconfig.lua_ls.setup {
   settings = { Lua = { diagnostics = { globals = {'vim'} } } },
   on_attach = nvlsp.on_attach,
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
 }
+
+lspconfig.ts_ls.setup {
+  single_file_support = false,
+  root_dir = lspconfig.util.root_pattern("package-lock.json"),
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+}
+
+lspconfig.denols.setup {
+  root_dir = lspconfig.util.root_pattern("deno.lock"),
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+}
+
+
